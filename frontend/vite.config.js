68bf37cda8f86d5f4d2d6038649e3bf1808ed2ba
awite <uniyhdemail@163.com>
2026-06-26 14:29:35 +0800

    修复前端 Sass 主题变量注入

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,12 @@ import uniPlugin from '@dcloudio/vite-plugin-uni'
 const uni = typeof uniPlugin === 'function' ? uniPlugin : uniPlugin.default
 
 export default defineConfig({
-  plugins: [uni()]
+  plugins: [uni()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "uview-plus/theme.scss" as *;\n'
+      }
+    }
+  }
 })
