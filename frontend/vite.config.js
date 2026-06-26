5684a3e4641e6d91f8fc1185bddfdfefc8b16026
awite <uniyhdemail@163.com>
2026-06-26 15:26:11 +0800

    兼容 HBuilder 云打包 Sass 配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,24 +2,7 @@ import { defineConfig } from 'vite'
 import uniPlugin from '@dcloudio/vite-plugin-uni'
 
 const uni = typeof uniPlugin === 'function' ? uniPlugin : uniPlugin.default
-const normalizePath = (path = '') => path.replace(/\\/g, '/')
-const withUViewTheme = (source, filename = '') => {
-  const normalized = normalizePath(filename)
-
-  if (normalized.endsWith('/src/uni.scss')) {
-    return source
-  }
-
-  return `@use "uview-plus/theme.scss" as *;\n${source}`
-}
 
 export default defineConfig({
-  plugins: [uni()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: withUViewTheme
-      }
-    }
-  }
+  plugins: [uni()]
 })
