4bd94c1cb9caf2d9238ddaeac87316a6baa6691a
awite <uniyhdemail@163.com>
2026-06-26 22:49:18 +0800

    重新打包前端APK并固定NDK版本

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 android {
     namespace = "com.manage.money.ai_account_book"
     compileSdk = flutter.compileSdkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
