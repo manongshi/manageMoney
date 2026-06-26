f99f0f3a1940c0dbedb3df9e38ccf898bcaf65b2
awite <uniyhdemail@163.com>
2026-06-26 15:20:32 +0800

    修复 HBuilder Sass use 顺序报错

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,24 @@ import { defineConfig } from 'vite'
 import uniPlugin from '@dcloudio/vite-plugin-uni'
 
 const uni = typeof uniPlugin === 'function' ? uniPlugin : uniPlugin.default
+const normalizePath = (path = '') => path.replace(/\\/g, '/')
+const withUViewTheme = (source, filename = '') => {
+  const normalized = normalizePath(filename)
+
+  if (normalized.endsWith('/src/uni.scss')) {
+    return source
+  }
+
+  return `@use "uview-plus/theme.scss" as *;\n${source}`
+}
 
 export default defineConfig({
-  plugins: [uni()]
+  plugins: [uni()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: withUViewTheme
+      }
+    }
+  }
 })
